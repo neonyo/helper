8cc0b8d54f960c4ecc058fbc60e1335634347db3
yangjiaming <jiaming.yang@tophant.com>
2022-06-14 16:52:02 +0800

    数组切块-去重

--- arrayunique.go
+++ arrayunique.go
@@ -25,3 +25,16 @@ func arrayUniqueInt(arr []int) []int {
 	}
 	return result
 }
+
+func arrayUnique(arr []interface{}) []interface{} {
+	size := len(arr)
+	result := make([]interface{}, 0, size)
+	temp := map[interface{}]struct{}{}
+	for i := 0; i < size; i++ {
+		if _, ok := temp[arr[i]]; ok != true {
+			temp[arr[i]] = struct{}{}
+			result = append(result, arr[i])
+		}
+	}
+	return result
+}
