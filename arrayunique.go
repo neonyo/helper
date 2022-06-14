fec41f955827bb4b36e4c48b51a75d5432556948
yangjiaming <jiaming.yang@tophant.com>
2022-06-14 16:50:38 +0800

    数组切块-去重

--- arrayunique.go
+++ arrayunique.go
@@ -1,9 +1,22 @@
 package helper
 
-func arrayUnique(arr []string) []string {
+func arrayUniqueString(arr []string) []string {
 	size := len(arr)
 	result := make([]string, 0, size)
 	temp := map[string]struct{}{}
+	for i := 0; i < size; i++ {
+		if _, ok := temp[arr[i]]; ok != true {
+			temp[arr[i]] = struct{}{}
+			result = append(result, arr[i])
+		}
+	}
+	return result
+}
+
+func arrayUniqueInt(arr []int) []int {
+	size := len(arr)
+	result := make([]int, 0, size)
+	temp := map[int]struct{}{}
 	for i := 0; i < size; i++ {
 		if _, ok := temp[arr[i]]; ok != true {
 			temp[arr[i]] = struct{}{}
