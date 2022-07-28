b0831a065b66330d4fe932089582a4a5c5408742
yangjiaming <jiaming.yang@tophant.com>
2022-07-28 13:17:31 +0800

    数组切块-去重

---
+++ arraymerge.go
@@ -0,0 +1,13 @@
+package helper
+
+func ArrayMerge(ss ...[]interface{}) []interface{} {
+	n := 0
+	for _, v := range ss {
+		n += len(v)
+	}
+	s := make([]interface{}, 0, n)
+	for _, v := range ss {
+		s = append(s, v...)
+	}
+	return s
+}
